0e7461bddb2e41044de78748cab621e668898c05
Ryan Trinkle <ryan.trinkle@gmail.com>
2013-01-28 11:45:28 -0500

    Make the site into a Snaplet.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,17 @@
 import Snap
 
-site :: Snap ()
-site = writeText "Hello, world!"
+data Memoise
+  = Memoise { }
+
+indexHandler :: Handler Memoise Memoise ()
+indexHandler = writeText "Hello, world!"
+
+memoiseInit :: SnapletInit Memoise Memoise
+memoiseInit = makeSnaplet "memoise" "The world's laziest hyperlink shortener" Nothing $ do
+  addRoutes [("", indexHandler)]
+  return $ Memoise { }
 
 main :: IO ()
-main = quickHttpServe site
+main = do
+  (_, site, _) <- runSnaplet Nothing memoiseInit
+  quickHttpServe site

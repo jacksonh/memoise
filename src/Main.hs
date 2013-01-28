eb00b95dc51a878a060b79cde5a9591816c676af
Ryan Trinkle <ryan.trinkle@gmail.com>
2013-01-28 11:57:29 -0500

    Add Heist templates.

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,24 @@
 import Snap
+import Snap.Snaplet.Heist
+import Control.Lens
 
 data Memoise
-  = Memoise { }
+  = Memoise { _heist :: Snaplet (Heist Memoise)
+            }
+makeLenses ''Memoise
+
+instance HasHeist Memoise where
+  heistLens = subSnaplet heist
 
 indexHandler :: Handler Memoise Memoise ()
-indexHandler = writeText "Hello, world!"
+indexHandler = render "index"
 
 memoiseInit :: SnapletInit Memoise Memoise
 memoiseInit = makeSnaplet "memoise" "The world's laziest hyperlink shortener" Nothing $ do
+  h <- nestSnaplet "heist" heist $ heistInit "templates"
   addRoutes [("", indexHandler)]
-  return $ Memoise { }
+  return $ Memoise { _heist = h
+                   }
 
 main :: IO ()
 main = do

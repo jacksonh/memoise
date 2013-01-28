73231564095af9467aded8bf77a3410dbaa854b4
Ryan Trinkle <ryan.trinkle@gmail.com>
2013-01-28 14:21:33 -0500

    Add a Heist splice to give feedback to the user.

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,12 @@ import Snap
 import Snap.Snaplet.Heist
 import Snap.Util.FileServe
 import Control.Lens
+import Data.Text
+import Heist.Interpreted
 
 data Memoise
   = Memoise { _heist :: Snaplet (Heist Memoise)
+            , _mainTextboxContents :: Maybe Text
             }
 makeLenses ''Memoise
 
@@ -15,16 +18,27 @@ indexHandler :: Handler Memoise Memoise ()
 indexHandler = do
   mUrl <- getParam "url"
   case mUrl of
-    Just url -> redirect url
+    Just url -> do
+      mainTextboxContents .= Just "URL Received.  What next?"
+      render "index"
     Nothing -> render "index"
+
+mainTextboxAttributeSplice :: Text -> Handler Memoise Memoise [(Text, Text)]
+mainTextboxAttributeSplice _ = do
+  mContents <- use mainTextboxContents
+  case mContents of
+    Just contents -> return [("value", contents)]
+    Nothing -> return []
 
 memoiseInit :: SnapletInit Memoise Memoise
 memoiseInit = makeSnaplet "memoise" "The world's laziest hyperlink shortener" Nothing $ do
   h <- nestSnaplet "heist" heist $ heistInit "templates"
+  modifyHeistState $ bindAttributeSplices [("main-textbox", mainTextboxAttributeSplice)]
   addRoutes [ ("static", serveDirectory "static")
             , ("", indexHandler)
             ]
   return $ Memoise { _heist = h
+                   , _mainTextboxContents = Nothing
                    }
 
 main :: IO ()
